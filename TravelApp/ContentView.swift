f7168078177ca3b1eba6fb1bde88be64a3127770
Rafael Torres <39065424+Raffatoo@users.noreply.github.com>
2021-07-15 11:33:39 -0500

    star project
    New project SwiftUI TravelApp

---
+++ TravelApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelApp
+//
+//  Created by Cruz Torres on 15/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Cruz Torres on 15/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
